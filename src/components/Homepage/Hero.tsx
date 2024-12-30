40d6c9def2d809b5e2c47ddc97feb5fb2953f1b7
zsolt-98 <zsolt.nagy998@gmail.com>
2024-12-30 12:21:14 +0100

    refactored code

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -50,87 +50,89 @@ export default function Hero() {
       fluid="xl"
       className="min-vh-100 px-0 bg-primary d-flex flex-column flex-xxl-row align-items-center justify-content-center gap-5"
     >
-      <div className="hero-main p-3 p-xl-5 mx-xl-0 rounded-5 d-flex flex-column flex-lg-row justify-content-between align-items-center position-relative text-center text-lg-start">
-        <div className="d-inline-block">
-          <div className="hero-heading-container">
-            <h3 className="text-info fs-4 fw-semibold mb-3">Hello! I'm</h3>
-            <div className="h1-cursor-wrapper">
-              <h1
-                className={`h1-cursor ${phase} d-inline-block text-white display-1 fw-light mb-3`}
-              >
-                {name}
-              </h1>
+      <div className="hero-main">
+        <div className="p-3 p-xl-5 mx-xl-0 rounded-5 d-flex flex-column flex-lg-row justify-content-between align-items-center position-relative text-center text-lg-start">
+          <div className="d-inline-block">
+            <div className="hero-heading-container">
+              <h3 className="text-info fs-4 fw-semibold mb-3">Hello! I'm</h3>
+              <div className="h1-cursor-wrapper">
+                <h1
+                  className={`h1-cursor ${phase} d-inline-block text-white display-1 fw-light mb-3`}
+                >
+                  {name}
+                </h1>
+              </div>
+              <h2 className="text-info fs-4 fw-semibold mb-3">
+                A front-end developer
+                <br />
+                based in Subotica, Serbia.
+              </h2>
             </div>
-            <h2 className="text-info fs-4 fw-semibold mb-3">
-              A front-end developer
-              <br />
-              based in Subotica, Serbia.
-            </h2>
+            <div className="hero-social-icon-container">
+              <a href="">
+                <IconLinkedin size={40} stroke="var(--bs-light)" />
+              </a>
+              <a href="">
+                <IconGithub size={40} stroke="var(--bs-light)" />
+              </a>
+            </div>
           </div>
-          <div className="hero-social-icon-container">
-            <a href="">
-              <IconLinkedin size={40} stroke="var(--bs-light)" />
-            </a>
-            <a href="">
-              <IconGithub size={40} stroke="var(--bs-light)" />
-            </a>
+          <div className="img-container position-relative mt-5 mt-lg-0">
+            <div className="hero-glowing-border rounded-5 border-info position-relative overflow-hidden">
+              <Image src={portfolioImage}></Image>
+            </div>
           </div>
         </div>
-        <div className="img-container position-relative mt-5 mt-lg-0">
-          <div className="hero-glowing-border rounded-5 border-info position-relative overflow-hidden">
-            <Image src={portfolioImage}></Image>
+        <div className="hero-tech-stack p-5 rounded-5 d-flex flex-column justify-content-start  position-relative">
+          <div className="">
+            <h3 className="text-info fs-4 fw-semibold mb-3 text-nowrap">
+              Tech stack
+            </h3>
           </div>
-        </div>
-      </div>
-      <div className="hero-tech-stack p-5 rounded-5 d-flex flex-column justify-content-center align-items-center position-relative">
-        <div className="">
-          <h3 className="text-info fs-4 fw-semibold mb-3 text-nowrap">
-            Tech stack
-          </h3>
-        </div>
-        <div className="hero-tech-stack-icons d-flex flex-column flex-md-row flex-xxl-column align-items-center">
-          <div className="d-flex flex-row flex-xxl-column">
-            <div className="d-flex">
-              <Image
-                className="hero-tech-icon"
-                src="https://skillicons.dev/icons?i=html"
-              />
-              <Image
-                className="hero-tech-icon"
-                src="https://skillicons.dev/icons?i=css"
-              />
+          <div className="hero-tech-stack-icons d-flex flex-row">
+            <div className="d-flex align-items-center">
+              <div className="d-flex flex-column flex-md-row">
+                <Image
+                  className="hero-tech-icon"
+                  src="https://skillicons.dev/icons?i=html"
+                />
+                <Image
+                  className="hero-tech-icon"
+                  src="https://skillicons.dev/icons?i=css"
+                />
+              </div>
+              <div className="d-flex flex-column flex-md-row">
+                <Image
+                  className="hero-tech-icon"
+                  src="https://skillicons.dev/icons?i=js"
+                />
+                <Image
+                  className="hero-tech-icon"
+                  src="https://skillicons.dev/icons?i=ts"
+                />
+              </div>
             </div>
-            <div className="d-flex">
-              <Image
-                className="hero-tech-icon"
-                src="https://skillicons.dev/icons?i=js"
-              />
-              <Image
-                className="hero-tech-icon"
-                src="https://skillicons.dev/icons?i=ts"
-              />
-            </div>
-          </div>
-          <div className="d-flex flex-row flex-xxl-column">
-            <div className="d-flex">
-              <Image
-                className="hero-tech-icon"
-                src="https://skillicons.dev/icons?i=react"
-              />
-              <Image
-                className="hero-tech-icon"
-                src="https://skillicons.dev/icons?i=bootstrap"
-              />
-            </div>
-            <div className="d-flex">
-              <Image
-                className="hero-tech-icon"
-                src="https://skillicons.dev/icons?i=sass"
-              />
-              <Image
-                className="hero-tech-icon"
-                src="https://skillicons.dev/icons?i=git"
-              />
+            <div className="d-flex ">
+              <div className="d-flex flex-column flex-md-row">
+                <Image
+                  className="hero-tech-icon"
+                  src="https://skillicons.dev/icons?i=react"
+                />
+                <Image
+                  className="hero-tech-icon"
+                  src="https://skillicons.dev/icons?i=bootstrap"
+                />
+              </div>
+              <div className="d-flex flex-column flex-md-row">
+                <Image
+                  className="hero-tech-icon"
+                  src="https://skillicons.dev/icons?i=sass"
+                />
+                <Image
+                  className="hero-tech-icon"
+                  src="https://skillicons.dev/icons?i=git"
+                />
+              </div>
             </div>
           </div>
         </div>
