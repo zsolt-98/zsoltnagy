821356045dcbbdb0f05ac956e5fb9109982039b5
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-11 16:38:47 +0100

    commit before attempting to change WorkAndVB background

--- src/components/Homepage/FooterHomepage.tsx
+++ src/components/Homepage/FooterHomepage.tsx
@@ -1,3 +1,9 @@
 export default function FooterHomepage() {
-  return <footer className="footer-wrapper min-vh-100"></footer>;
+  return (
+    <footer className="footer-wrapper min-vh-100 position-relative">
+      <div className="footer-bg-container">
+        <div className="footer-bg-grain"></div>
+      </div>
+    </footer>
+  );
 }
