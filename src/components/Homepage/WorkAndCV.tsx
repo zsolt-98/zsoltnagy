0b1d9c9fef3f3079dbf6b18a5c363cf9112e6850
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-11 12:08:30 +0100

    added main content to workandcv

--- src/components/Homepage/WorkAndCV.tsx
+++ src/components/Homepage/WorkAndCV.tsx
@@ -1,6 +1,8 @@
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 import PageDivider from "../SVG/PageDivider";
+import IconArrowRight from "../Icons/IconArrowRight";
 
 export default function WorkAndCV() {
   return (
@@ -10,22 +12,47 @@ export default function WorkAndCV() {
         <PageDivider />
       </div>
       <Container fluid="md">
-        <div className="p-3 p-xl-5 d-flex justify-content-evenly bg-primary">
-          <div className="">
-            <h2 className="text-capitalize text-info display-4 fw-bold">
-              My work
-            </h2>
-            <p className="text-light fs-4">
-              A showcase of my latest web projects.
-            </p>
+        <div className="d-flex justify-content-evenly bg-primary position-relative gap-6">
+          <div className="d-flex flex-column gap-6 p-3 p-xl-5 w-50">
+            <div className="">
+              <h2 className="text-capitalize text-info display-4 fw-bold">
+                My work
+              </h2>
+              <p className="text-light fs-4">
+                A showcase of my latest web projects.
+              </p>
+            </div>
+            <Button
+              variant="outline-info"
+              size="lg"
+              className="rounded-0 text-uppercase fs-6 border-2 w-75"
+            >
+              See my work
+              <span className="ms-4">
+                <IconArrowRight className="workcv-arrow-right" />
+              </span>
+            </Button>
           </div>
-          <div className="">
-            <h2 className="text-capitalize text-info display-4 fw-bold">
-              My résumé
-            </h2>
-            <p className="text-light fs-4">
-              See my professional experience and skills.
-            </p>
+          <div className="workcv-divider bg-info text-info opacity-100 position-absolute top-50 start-50 translate-middle" />
+          <div className="d-flex flex-column gap-6 p-3 p-xl-5 w-50">
+            <div className="">
+              <h2 className="text-capitalize text-info display-4 fw-bold">
+                My résumé
+              </h2>
+              <p className="text-light fs-4">
+                See my professional experience and skills.
+              </p>
+            </div>
+            <Button
+              variant="outline-info"
+              size="lg"
+              className="rounded-0 text-uppercase fs-6 border-2 w-75"
+            >
+              See my résumé
+              <span className="ms-4">
+                <IconArrowRight className="workcv-arrow-right" />
+              </span>
+            </Button>
           </div>
         </div>
       </Container>
---
+++ src/components/Icons/IconArrowRight.tsx
@@ -0,0 +1,21 @@
+import { IconProps } from "../../types";
+
+export default function IconArrowRight({ className }: IconProps) {
+  return (
+    <svg
+      width="72"
+      height="22"
+      viewBox="0 0 72 22"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        fill="none"
+        stroke="#58c4dc"
+        stroke-width="3"
+        stroke-miterlimit="0"
+        d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"
+      ></path>
+    </svg>
+  );
+}
