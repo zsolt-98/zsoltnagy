439e2ecbec97760ff3a141199a7129f979761c26
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-11 19:32:28 +0100

    netlify push

--- src/components/Homepage/FooterHomepage.tsx
+++ src/components/Homepage/FooterHomepage.tsx
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Stack from "react-bootstrap/Stack";
 import Nav from "react-bootstrap/Nav";
 import IconLinkedin from "../Icons/IconLinkedin";
 import IconGithub from "../Icons/IconGithub";
