339077ed1921a3f3e92d48161ee46fae7ec28562
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-14 15:56:55 +0100

    started adding carousel container

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -1,19 +1,25 @@
 import Container from "react-bootstrap/Container";
 import Navigation from "../Navigation";
+import Image from "react-bootstrap/Image";
 
 import bgWork from "../../assets/bg-work.png";
+import test from "../../assets/yourney-linkedin.jpg";
 
 export default function Work() {
   return (
     <section className="min-vh-100 position-relative d-flex justify-content-between">
-      <img
+      <Image
         className="position-absolute w-100 h-100 z-1 opacity-25 py-6"
         src={bgWork}
       />
       <div className="work-bg-container position-absolute">
         <div className="work-bg-grain position-absolute" />
       </div>
-      <Container></Container>
+      <Container className="d-flex justify-content-center align-items-center z-2">
+        <div className="work-carousel-container w-100 position-relative px-5 py-4">
+          <Image src={test} className="w-100 h-100" />
+        </div>
+      </Container>
       <Navigation />
     </section>
   );
