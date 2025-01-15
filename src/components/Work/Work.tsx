5e1ed2657aae6e3e2bb794930234bced41855e3f
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-15 12:27:28 +0100

    commit before adding hoverable image

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -18,33 +18,37 @@ export default function Work() {
       </div>
       <Container className="work-project-container d-flex justify-content-center align-items-center flex-column z-2 gap-6 my-6">
         <div className="text-center">
-          <h2 className="text-light display-3">
+          <h2 className="text-light display-4">
             <span className="text-info">/</span>work
             <span className="text-info ms-1">.</span>
           </h2>
-          <p className="text-info">
+          <p className="text-info fs-5">
             A collection of the projects that I have worked on.
           </p>
         </div>
         <div className="d-flex gap-3">
-          <div className="">
-            <div className="work-project-img d-flex justify-content-center align-items-center bg-light p-6">
-              <Image src={yourneyLogo} className=" "></Image>
-            </div>
-            <div className="work-project-text p-4">
-              <h3 className="text-info">Yourney</h3>
-              <p className="mb-0 text-light">your-ney.netlify.app</p>
-            </div>
-          </div>
-          <div className="">
-            <div className="work-project-img d-flex justify-content-center align-items-center bg-light p-6 ">
-              <Image src={portfolioLogo} className=""></Image>
-            </div>
-            <div className="work-project-text p-4">
-              <h3 className="text-info">Personal Website</h3>
-              <p className="mb-0 text-light">zsn.guru</p>
-            </div>
-          </div>
+          <button className="border-0 bg-transparent p-0">
+            <figure className="m-0">
+              <div className="work-project-img d-flex justify-content-center align-items-center bg-light p-6">
+                <Image src={yourneyLogo} className="" />
+              </div>
+              <figcaption className="work-project-text p-4">
+                <h3 className="text-info">Yourney</h3>
+                <p className="mb-0 text-light">your-ney.netlify.app</p>
+              </figcaption>
+            </figure>
+          </button>
+          <button className="border-0 bg-transparent p-0">
+            <figure className="m-0">
+              <div className="work-project-img d-flex justify-content-center align-items-center bg-light p-6 ">
+                <Image src={portfolioLogo} className=""></Image>
+              </div>
+              <figcaption className="work-project-text p-4">
+                <h3 className="text-info">Personal Website</h3>
+                <p className="mb-0 text-light">zsn.guru</p>
+              </figcaption>
+            </figure>
+          </button>
         </div>
       </Container>
       <Navigation />
