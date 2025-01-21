78d80cc0fb7ef338d4368b2adadc83a37d475832
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-21 19:54:24 +0100

    working on /resume

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Homepage from "./components/Homepage/Homepage";
 import { Route, BrowserRouter as Router, Routes } from "react-router";
 import Work from "./components/Work/Work";
 import Yourney from "./components/Work/Yourney";
+import Resume from "./components/Resume/Resume";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="work" element={<Work />}>
             <Route path="yourney" element={<Yourney />} />
           </Route>
+          <Route path="resume" element={<Resume />}></Route>
         </Routes>
       </Router>
     </>
---
+++ src/components/Resume/Resume.tsx
@@ -0,0 +1,153 @@
+import Container from "react-bootstrap/Container";
+import IconLinkedin from "../Icons/IconLinkedin";
+import IconGithub from "../Icons/IconGithub";
+import IconLocation from "../Icons/IconLocation";
+import IconEmail from "../Icons/IconEmail";
+import IconUser from "../Icons/IconUser";
+
+export default function Resume() {
+  return (
+    <section className="position-relative w-100 vh-100">
+      <div className="resume-bg-container position-absolute">
+        <div className="resume-bg-grain position-absolute" />
+      </div>
+      <Container
+        fluid="md"
+        className={`resume-container position-relative d-flex
+         justify-content-center align-items-center px-0 w-100 h-100 z-1`}
+      >
+        <div className="d-flex flex-column">
+          <h2 className="fs-1 text-info">Zsolt Nagy</h2>
+          <div className="d-flex gap-4 text-light">
+            <p className="fs-6 fw-normal">
+              <span>
+                <IconLocation
+                  className="me-1"
+                  size={22}
+                  stroke="none"
+                  fill="#e5e7eb"
+                />
+              </span>
+              Subotica, Serbia
+            </p>
+            <p className="fs-6 fw-normal">
+              <span>
+                <IconEmail
+                  className="me-1"
+                  size={22}
+                  stroke="none"
+                  fill="#e5e7eb"
+                />
+              </span>
+              zsolt.nagy998@gmail.com
+            </p>
+            <p className="fs-6 fw-normal">
+              <span>
+                <IconGithub className="me-1" size={25} stroke="#e5e7eb" />
+              </span>
+              zsolt-98
+            </p>
+            <p className="fs-6 fw-normal">
+              <span>
+                <IconLinkedin className="me-1" size={25} stroke="#e5e7eb" />
+              </span>
+              zsolt98
+            </p>
+            <p className="fs-6 fw-normal">
+              <span>
+                <IconUser
+                  className="me-1"
+                  size={22}
+                  stroke="none"
+                  fill="#e5e7eb"
+                />
+              </span>
+              www.zsn.guru
+            </p>
+          </div>
+          <p className="text-light">
+            Web developer with a passion for intuitive design and strong
+            problem-solving abilities. Specializes infrontend development with
+            expertise in JavaScript and ReactJS, crafting responsive and
+            user-focusedweb applications.
+          </p>
+          <div className="border-top border-info" />
+          <h3 className="fs-2 text-info">Professional Experience</h3>
+          <h4 className="fs-3 text-info fw-normal">Employment summary</h4>
+          <h5 className="fs-4 text-info fst-italic fw-light">
+            Work history across the United Kingdom and Serbia
+          </h5>
+          <ul className="text-light fs-6 fw-normal">
+            <li className="">
+              Managed and maintained multiple Shopify e-commerce websites,
+              including content updates, themecustomization, and code
+              implementation.
+            </li>
+            <li className="">
+              Optimized Meta Ads and Google Ads campaigns, improving conversion
+              rates and site traffic.
+            </li>
+            <li className="">
+              Provided customer support with a proven track record of high
+              satisfaction ratings and successfulupselling initiatives.
+            </li>
+          </ul>
+
+          <h3 className="fs-2 text-info">Projects</h3>
+          <h4 className="fs-3 text-info fw-normal">Yourney</h4>
+          <h5 className="fs-4 text-info fst-italic fw-light">
+            https://your-ney.netlify.app/
+          </h5>
+          <ul className="text-light fs-6 fw-normal">
+            <li className="">
+              Developed a premium car rental platform using ReactJS, JavaScript,
+              Bootstrap, and Sass withZustand for state management.
+            </li>
+            <li className="">
+              Implemented complete user authentication flow including
+              registration, sign-in, and password resetfunctionality.
+            </li>
+            <li className="">
+              Built comprehensive booking system with vehicle selection, time
+              and location scheduling.
+            </li>
+            <li className="">
+              Created user profile system with frontend CRUD operations for
+              profile management and bookingcreation/deletion.
+            </li>
+          </ul>
+
+          <h3 className="fs-2 text-info">Education</h3>
+          <h4 className="fs-3 text-info fw-normal">
+            Technical Secondary School "Ivan Sarić"
+          </h4>
+          <h5 className="fs-4 text-info fst-italic fw-light">
+            Information Technology (IT) Technician
+          </h5>
+          <ul className="text-light fs-6 fw-normal">
+            <li className="">
+              Technical education with a focus on computer science, including
+              programming languages, webdevelopment, and database management.
+            </li>
+          </ul>
+
+          <h3 className="fs-2 text-info">Skills</h3>
+          <div className="d-flex gap-6">
+            <div className="text-light fs-6 fw-normal ">
+              <p className="m-0">JavaScript</p>
+              <p className="m-0">Web</p>
+              <p className="m-0">Design</p>
+            </div>
+            <div className="text-light">
+              <p className="m-0">ReactJS, TypeScript</p>
+              <p className="m-0">
+                HTML, CSS, Sass, Bootstrap, PostgreSQL, Git, Citrix
+              </p>
+              <p className="m-0">Figma, Canva</p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
