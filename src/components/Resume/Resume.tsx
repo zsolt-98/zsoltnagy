60a7f0a4a320db4bedfea3a929a849d9c0e3b465
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-25 15:31:46 +0100

    added separate language files to resume

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,21 +1,20 @@
 import Container from "react-bootstrap/Container";
-import IconLinkedin from "../Icons/IconLinkedin";
-import IconGithub from "../Icons/IconGithub";
-import IconLocation from "../Icons/IconLocation";
-import IconEmail from "../Icons/IconEmail";
-import IconUser from "../Icons/IconUser";
 
-import Table from "react-bootstrap/Table";
 import Navigation from "../Navigation";
 import IconDownload from "../Icons/IconDownload";
 
 import ResumePDF from "../../assets/Zsolt_Nagy_CV_Eng.pdf";
 import useZustandStore from "../../store/useZustandStore";
 import { useMediaQuery } from "react-responsive";
+import { useState } from "react";
+import ResumeEng from "./ResumeEng";
+import ResumeSrb from "./ResumeSrb";
+import ResumeHun from "./ResumeHun";
 
 export default function Resume() {
   const isXXLargeScreen = useMediaQuery({ minWidth: 1500 });
   const { isOpen } = useZustandStore();
+  const [currentLang, setCurrentLang] = useState("eng");
 
   return (
     <>
@@ -36,11 +35,32 @@ export default function Resume() {
             <h2 className="mb-4 fs-1 text-info">Zsolt Nagy</h2>
             <div className="resume-language-download p-3 position-absolute d-flex gap-4">
               <div className="resume-language d-flex gap-1 text-light fw-light">
-                <p className="">ENG</p>
+                <button
+                  className="bg-transparent border-0 text-light"
+                  onClick={() => {
+                    setCurrentLang("eng");
+                  }}
+                >
+                  ENG
+                </button>
                 <span>|</span>
-                <p className="">HUN</p>
+                <button
+                  className="bg-transparent border-0 text-light"
+                  onClick={() => {
+                    setCurrentLang("srb");
+                  }}
+                >
+                  SRB
+                </button>
                 <span>|</span>
-                <p className="">SRB</p>
+                <button
+                  className="bg-transparent border-0 text-light"
+                  onClick={() => {
+                    setCurrentLang("hun");
+                  }}
+                >
+                  HUN
+                </button>
               </div>
               <a
                 className="resume-download text-light text-decoration-none"
@@ -58,172 +78,9 @@ export default function Resume() {
                 Download
               </a>
             </div>
-            <div className="d-flex flex-wrap gap-4 text-light mb-4">
-              <p className="fs-6 fw-normal mb-0">
-                <span>
-                  <IconLocation
-                    className="me-1"
-                    size={22}
-                    stroke="none"
-                    fill="#e5e7eb"
-                  />
-                </span>
-                Subotica, Serbia
-              </p>
-              <a
-                className="resume-link-fill fs-6 text-light fw-normal text-decoration-none mb-0"
-                href="mailto:zsolt.nagy998@gmail.com"
-              >
-                <span>
-                  <IconEmail
-                    className="me-1"
-                    size={22}
-                    stroke="none"
-                    fill="#e5e7eb"
-                  />
-                </span>
-                zsolt.nagy998@gmail.com
-              </a>
-              <a
-                className="resume-link-stroke fs-6 text-light fw-normal text-decoration-none mb-0"
-                href="https://github.com/zsolt-98"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <IconGithub className="me-1" size={25} stroke="#e5e7eb" />
-                </span>
-                zsolt-98
-              </a>
-              <a
-                className="resume-link-stroke fs-6 text-light fw-normal text-decoration-none mb-0"
-                href="https://linkedin.com/in/zsolt98"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <IconLinkedin className="me-1" size={25} stroke="#e5e7eb" />
-                </span>
-                zsolt98
-              </a>
-              <a
-                className="resume-link-fill fs-6 text-light fw-normal text-decoration-none mb-0"
-                href="https://zsn.guru"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <IconUser
-                    className="me-1"
-                    size={22}
-                    stroke="none"
-                    fill="#e5e7eb"
-                  />
-                </span>
-                www.zsn.guru
-              </a>
-            </div>
-            <p className="text-light mb-0">
-              Web developer with strong problem-solving abilities and a passion
-              for intuitive design. Specializes in frontend development with
-              expertise in JavaScript and ReactJS, crafting responsive and
-              user-focused web applications.
-            </p>
-            <div className="my-4 resume-divider border-top border-info" />
-            <h3 className="fs-2 text-info mb-4">Professional Experience</h3>
-            <h4 className="fs-3 text-info fw-normal mb-2">
-              Employment summary
-            </h4>
-            <h5 className="fs-4 text-info fst-italic fw-light mb-2">
-              Work history across the United Kingdom and Serbia
-            </h5>
-            <ul className="text-light fs-6 fw-normal mb-0">
-              <li className="">
-                Managed and maintained multiple Shopify e-commerce websites,
-                including content updates, theme customization, and code
-                implementation.
-              </li>
-              <li className="">
-                Optimized Meta Ads and Google Ads campaigns, improving
-                conversion rates and site traffic.
-              </li>
-              <li className="">
-                Provided customer support with a proven track record of high
-                satisfaction ratings and successful upselling initiatives.
-              </li>
-            </ul>
-
-            <h3 className="fs-2 text-info my-4">Projects</h3>
-            <h4 className="fs-3 text-info fw-normal mb-2">Yourney</h4>
-            <h5 className="fs-4 text-info fst-italic fw-light mb-2">
-              https://your-ney.netlify.app/
-            </h5>
-            <ul className="text-light fs-6 fw-normal mb-0">
-              <li className="">
-                Developed a premium car rental platform using ReactJS,
-                JavaScript, Bootstrap, and Sass withZustand for state
-                management.
-              </li>
-              <li className="">
-                Implemented complete user authentication flow including
-                registration, sign-in, and password reset functionality.
-              </li>
-              <li className="">
-                Built comprehensive booking system with vehicle selection, time
-                and location scheduling.
-              </li>
-              <li className="">
-                Created user profile system with frontend CRUD operations for
-                profile management and booking creation/deletion.
-              </li>
-            </ul>
-
-            <h3 className="fs-2 text-info my-4">Education</h3>
-            <h4 className="fs-3 text-info fw-normal mb-2">
-              Technical Secondary School "Ivan Sarić"
-            </h4>
-            <h5 className="fs-4 text-info fst-italic fw-light mb-2">
-              Information Technology (IT) Technician
-            </h5>
-            <ul className="text-light fs-6 fw-normal mb-0">
-              <li className="">
-                Technical education with a focus on computer science, including
-                programming languages, web development, and database management.
-              </li>
-            </ul>
-
-            <h3 className="fs-2 text-info my-4">Skills</h3>
-            <div className="table-responsive">
-              <Table responsive borderless className="resume-table">
-                <tbody>
-                  <tr>
-                    <td className="p-0 pe-6 fs-6 fw-normal text-light ">
-                      JavaScript
-                    </td>
-                    <td className="p-0 fs-6 fw-normal text-light">
-                      ReactJS, TypeScript
-                    </td>
-                  </tr>
-
-                  <tr>
-                    <td className="p-0 pe-6 fs-6 fw-normal text-light ">Web</td>
-                    <td className="p-0 fs-6 fw-normal text-light">
-                      HTML, CSS, Sass, Bootstrap, PostgreSQL, Git, Shopify,
-                      Citrix
-                    </td>
-                  </tr>
-
-                  <tr>
-                    <td className="p-0 pe-6 fs-6 fw-normal text-light ">
-                      Design
-                    </td>
-                    <td className="p-0 fs-6 fw-normal text-light">
-                      Figma, Canva
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
-            </div>
+            {currentLang === "eng" && <ResumeEng />}
+            {currentLang === "srb" && <ResumeSrb />}
+            {currentLang === "hun" && <ResumeHun />}
           </div>
         </Container>
         <Navigation />
---
+++ src/components/Resume/ResumeHun.tsx
@@ -0,0 +1,162 @@
+import IconLinkedin from "../Icons/IconLinkedin";
+import IconGithub from "../Icons/IconGithub";
+import IconLocation from "../Icons/IconLocation";
+import IconEmail from "../Icons/IconEmail";
+import IconUser from "../Icons/IconUser";
+
+import Table from "react-bootstrap/Table";
+
+export default function ResumeHun() {
+  return (
+    <>
+      <div className="d-flex flex-wrap gap-4 text-light mb-4">
+        <p className="fs-6 fw-normal mb-0">
+          <span>
+            <IconLocation
+              className="me-1"
+              size={22}
+              stroke="none"
+              fill="#e5e7eb"
+            />
+          </span>
+          Szabadka, Szerbia
+        </p>
+        <a
+          className="resume-link-fill fs-6 text-light fw-normal text-decoration-none mb-0"
+          href="mailto:zsolt.nagy998@gmail.com"
+        >
+          <span>
+            <IconEmail
+              className="me-1"
+              size={22}
+              stroke="none"
+              fill="#e5e7eb"
+            />
+          </span>
+          zsolt.nagy998@gmail.com
+        </a>
+        <a
+          className="resume-link-stroke fs-6 text-light fw-normal text-decoration-none mb-0"
+          href="https://github.com/zsolt-98"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>
+            <IconGithub className="me-1" size={25} stroke="#e5e7eb" />
+          </span>
+          zsolt-98
+        </a>
+        <a
+          className="resume-link-stroke fs-6 text-light fw-normal text-decoration-none mb-0"
+          href="https://linkedin.com/in/zsolt98"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>
+            <IconLinkedin className="me-1" size={25} stroke="#e5e7eb" />
+          </span>
+          zsolt98
+        </a>
+        <a
+          className="resume-link-fill fs-6 text-light fw-normal text-decoration-none mb-0"
+          href="https://zsn.guru"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>
+            <IconUser className="me-1" size={22} stroke="none" fill="#e5e7eb" />
+          </span>
+          www.zsn.guru
+        </a>
+      </div>
+      <p className="text-light mb-0">
+        Erős problémamegoldó képességgel és az intuitív dizájn iránt
+        szenvedéllyel rendelkező webfejlesztő. Reszponzív és felhasználóközpontú
+        frontend webalkalmazások készítésére specializálódott, JavaScript és
+        ReactJS szakértelemmel.
+      </p>
+      <div className="my-4 resume-divider border-top border-info" />
+      <h3 className="fs-2 text-info mb-4">Munkahelyi tapasztalat</h3>
+      <h4 className="fs-3 text-info fw-normal mb-2">Munkatörténet</h4>
+      <h5 className="fs-4 text-info fst-italic fw-light mb-2">
+        Az Egyesült Királyságban és Szerbiában
+      </h5>
+      <ul className="text-light fs-6 fw-normal mb-0">
+        <li className="">
+          Több Shopify e-kereskedelmi weboldal kezelése és karbantartása,
+          beleértve a tartalomfrissítéseket, a téma testreszabását és kód
+          implementációt.
+        </li>
+        <li className="">
+          Meta Ads és Google Ads kampányok optimalizálása, a konverziós arány és
+          a forgalom növelése érdekében.
+        </li>
+        <li className="">
+          Ügyféltámogatás nyújtása bizonyítottan magas elégedettségi mutatókkal
+          és sikeres felülértékesítési kezdeményezésekkel.
+        </li>
+      </ul>
+      <h3 className="fs-2 text-info my-4">Projektek</h3>
+      <h4 className="fs-3 text-info fw-normal mb-2">Yourney</h4>
+      <h5 className="fs-4 text-info fst-italic fw-light mb-2">
+        https://your-ney.netlify.app/
+      </h5>
+      <ul className="text-light fs-6 fw-normal mb-0">
+        <li className="">
+          Prémium autókölcsönző platform kifejlesztése, ReactJS, JavaScript,
+          Bootstrap, Sass és Zustand eszközök használatával.
+        </li>
+        <li className="">
+          Teljes felhasználó hitelesítési folyamat kiépítése, beleértve a
+          regisztrációs, bejelentkezési és jelszó-visszaállítási funkciókat.
+        </li>
+        <li className="">
+          Széleskörű foglalási rendszer kifejlesztése járműválasztással, idő- és
+          helymeghatározással.
+        </li>
+        <li className="">
+          Felhasználói profil létrehozása ahol a profilkezelés és a foglalások
+          létrehozásá/törlésé frontend CRUD műveletekkel kivitelezhető.
+        </li>
+      </ul>
+      <h3 className="fs-2 text-info my-4">Oktatás</h3>
+      <h4 className="fs-3 text-info fw-normal mb-2">
+        "Ivan Sarić" Műszaki Középiskola
+      </h4>
+      <h5 className="fs-4 text-info fst-italic fw-light mb-2">
+        Informatikai (IT) technikus
+      </h5>
+      <ul className="text-light fs-6 fw-normal mb-0">
+        <li className="">
+          Műszaki oktatás, amelynek középpontjában az informatika áll, beleértve
+          a programozási nyelveket, a webfejlesztést és az adatbázis-kezelést.
+        </li>
+      </ul>
+      <h3 className="fs-2 text-info my-4">Képességek</h3>
+      <div className="table-responsive">
+        <Table responsive borderless className="resume-table">
+          <tbody>
+            <tr>
+              <td className="p-0 pe-6 fs-6 fw-normal text-light ">
+                JavaScript
+              </td>
+              <td className="p-0 fs-6 fw-normal text-light">
+                ReactJS, TypeScript
+              </td>
+            </tr>
+            <tr>
+              <td className="p-0 pe-6 fs-6 fw-normal text-light ">Web</td>
+              <td className="p-0 fs-6 fw-normal text-light">
+                HTML, CSS, Sass, Bootstrap, PostgreSQL, Git, Shopify, Citrix
+              </td>
+            </tr>
+            <tr>
+              <td className="p-0 pe-6 fs-6 fw-normal text-light ">Dizájn</td>
+              <td className="p-0 fs-6 fw-normal text-light">Figma, Canva</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    </>
+  );
+}
